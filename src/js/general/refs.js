af8f76a0b5fc1f06d0c7905466f500232930514e
Yuri Belebekha <ybelebekha@gmail.com>
2023-08-12 00:08:55 +0300

    Added h2 (hidden) for Gallery section and deleted commented code

--- src/js/general/refs.js
+++ src/js/general/refs.js
@@ -1,5 +1,4 @@
 export const refs = {
   // HOME PAGE
   galleryTrendingMovies: document.querySelector('.gallery-trending-movies__list'),
-  // paginationList: document.querySelector('.pagination__list'),
 };
--- src/js/render/home/gallery-trending-movies.js
+++ src/js/render/home/gallery-trending-movies.js
@@ -17,7 +17,6 @@ $('[js-pagination-home-gallery]').pagination({
   dataSource: getArrayPageNumbers(),
   pageSize: 1,
   pageRange: 1,
-  // autoHidePrevious: true,
   showGoInput: true,
   showGoButton: true,
 
@@ -103,95 +102,3 @@ function getGenreMovie(genreId) {
     };
   };
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import {
-//   fetchGetTrending,
-//   fetchGenreMovieList,
-//   baseApiUrlForPoster,
-//   posterWidth,
-//   posterHeight,
-//   posterMissing,
-// } from '../../services/tmdb-api';
-// import { refs } from '../../general/refs';
-
-// // Fetching and rendering trending movies
-// fetchGetTrending()
-//   .then(({ results }) => {
-//     if (!results) {
-//       return noGalleryTrendingMoviesMarkup();
-//     };
-
-//     if (results) {
-//       createGalleryTrendingMovies(results);
-//     };
-//   })
-//   .catch(error => console.error(error));
-
-// function noGalleryTrendingMoviesMarkup() {
-//   refs.galleryTrendingMovies.innerHTML = '<li>Something went wrong... Please, try again later.</li>';
-// };
-
-// function createGalleryTrendingMovies(data) {
-//   const markup = data.map(({ genre_ids, title, poster_path, release_date }) => {
-//     let genreMovie = [];
-//     genre_ids.map((genre) => { genreMovie.push(getGenreMovie(genre)) });
-//     let normalizedStringGenreMovie = genreMovie.join(', ');    
-    
-//     let releaseYear = release_date ? release_date.slice(0, 4) : 'N/A';
-
-//     return `
-//       <li class="gallery-trending-movies__item">
-//         <img
-//           class="gallery-trending-movies__poster"
-//           src="${poster_path ? baseApiUrlForPoster.concat(poster_path) : posterMissing}"
-//           alt="${title}"
-//           width="${posterWidth}"
-//           height="${posterHeight}"
-//           loading="lazy"
-//         />
-//         <div class="gallery-trending-movies__description">
-//           <h2 class="gallery-trending-movies__name">${title}</h2>
-//           <p class="gallery-trending-movies__genre">${normalizedStringGenreMovie} | ${releaseYear}</p>
-//         </div>
-//       </li>
-//     `
-//   }).join('');
-
-//   refs.galleryTrendingMovies.insertAdjacentHTML('beforeend', markup);
-// };
-
-// // Fetching and converting genre id to genre name text
-// fetchGenreMovieList()
-//   .then(({ genres }) => saveGenresListToLocalStorage(genres));
-
-// function saveGenresListToLocalStorage(data) {
-//   localStorage.setItem('genres', JSON.stringify(data));
-// };
-
-// function getGenreMovie(genreId) {
-//   let result = localStorage.getItem('genres');
-
-//   for (const elem of JSON.parse(result)) {
-//     if (elem.id === genreId) {
-//       return elem.name;
-//     };
-//   };
-// };
