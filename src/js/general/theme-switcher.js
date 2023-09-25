e5312bfd480cf41d35b6f9d0a66edf00a8899f53
Yuri Belebekha <ybelebekha@gmail.com>
2023-09-26 00:03:48 +0300

    Added theme to localStorage

--- src/js/general/theme-switcher.js
+++ src/js/general/theme-switcher.js
@@ -1,11 +1,23 @@
-import { refs } from './refs';
+import { refs } from './refs';	
 
-Array.from(refs.radiosThemeSwitcher).forEach( (elem, _i) => {		
+Array.from(refs.radiosThemeSwitcher).forEach((elem, _i) => {		
+	const getThemeValue = localStorage.getItem('theme');
+
+	if (getThemeValue === elem.attributes.value.textContent) {
+		elem.setAttribute('checked', '');
+		document.querySelector('main').setAttribute('data-theme', getThemeValue);
+		document.querySelector('footer').setAttribute('data-theme', getThemeValue);
+	};
+
 	elem.addEventListener('change', e => {
 		if(e.target.checked) {
 			let value = e.target.getAttribute('value');
-			document.querySelector('main').setAttribute('data-theme', value);
-			document.querySelector('footer').setAttribute('data-theme', value);
+			
+			localStorage.setItem('theme', value);
+			const getThemeValue = localStorage.getItem('theme');
+
+			document.querySelector('main').setAttribute('data-theme', getThemeValue);
+			document.querySelector('footer').setAttribute('data-theme', getThemeValue);
 			document.documentElement.classList.add('in-transition');
 			document.documentElement.classList.remove('color-theme-in-transition');			
 		};
